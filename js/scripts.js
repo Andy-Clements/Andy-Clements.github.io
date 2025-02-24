eb866173a74d38e1c2b464e41e282c52b90881c9
Andy Clements <44131172+Andy-Clements@users.noreply.github.com>
2025-02-24 07:27:49 -0700

    Update scripts.js
    just adding some notes inside the javascript for potential future functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
+/* 
+    Fetching the data JavaScript code
+*/
 const apiKey= 'AIzaSyDbJOqAmIdtmfTKGKztuto-LXEFxBqTtmo'; // API key created from console.cloud.google.com
 const sheetId = '1DpkzKMnE8io4rmBz2uc8KfclJm4FpZnXazk2BacpupA'; // the id of the spreadsheet labelled like: https://docs.google.com/spreadsheets/d/1DpkzKMnE8io4rmBz2uc8KfclJm4FpZnXazk2BacpupA/edit?gid=2122775497#gid=2122775497
 const sheetName = 'TestData01'; // Name of the sheet(tab in case multiple tabs) we want to pull the data from
 const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${apiKey}`; //Google sheets API v4 URL
 
-// Fetch data from the Google Sheet API
+    /* 
+        Fetch data from the Google Sheet API
+        This comes with selected columns and auto scrolls on reload to the left
+    */
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
@@ -45,3 +51,13 @@ fetch(apiUrl)
         }
     })
     .catch(error => console.error('Error fetching data: ', error));
+
+/* 
+    Filter the data
+*/
+
+
+
+/*
+    Sort the data
+*/
